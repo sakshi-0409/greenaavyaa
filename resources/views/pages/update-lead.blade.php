69365ea54f881c6348829c5440814b7fca3692f5
sakshi-0409 <sakshigoyal2019@gmail.com>
2023-08-02 16:01:26 +0530

    update-leads option old value issue fixed

--- resources/views/pages/update-lead.blade.php
+++ resources/views/pages/update-lead.blade.php
@@ -20,66 +20,66 @@
           <label for="leadsource" class="form-label">Lead Source</label>
             <select name="leadsource" id="leadsource" class="form-select" aria-label="Default select example">
                 <option selected>Open this select menu</option>
-                <option value="Social Media">Social Media</option>
-                <option value="Digital Marketing">Digital Marketing</option>
-                <option value="Networking">Networking</option>
-                <option value="Website">Website</option>
-                <option value="Email">Email</option>
-                <option value="SMS">SMS</option>
-                <option value="Whatsapp">Whatsapp</option>
-                <option value="Campaign">Campaign</option>
-                <option value="Fairs">Fairs</option>
-                <option value="others">Others</option>
+                <option value="Social Media" {{@$lead->source === 'Social Media' ? 'selected': ''}}>Social Media</option>
+                <option value="Digital Marketing" {{@$lead->source === 'Digital Marketing' ? 'selected': ''}}>Digital Marketing</option>
+                <option value="Networking" {{@$lead->source === 'Networking' ? 'selected': ''}}>Networking</option>
+                <option value="Website" {{@$lead->source === 'Website' ? 'selected': ''}}>Website</option>
+                <option value="Email" {{@$lead->source === 'Email' ? 'selected': ''}}>Email</option>
+                <option value="SMS" {{@$lead->source === 'SMS' ? 'selected': ''}}>SMS</option>
+                <option value="Whatsapp" {{@$lead->source === 'Whatsapp' ? 'selected': ''}}>Whatsapp</option>
+                <option value="Campaign" {{@$lead->source === 'Campaign' ? 'selected': ''}}>Campaign</option>
+                <option value="Fairs" {{@$lead->source === 'Fairs' ? 'selected': ''}}>Fairs</option>
+                <option value="others" {{@$lead->source === 'others' ? 'selected': ''}}>Others</option>
               </select>
         </div>
         <div class="col-3">
           <label for="leadby" class="form-label">Lead By</label>
             <select name="leadby" id="leadby" class="form-select" aria-label="Default select example">
                 <option selected>Open this select menu</option>
-                <option value="Broker">Broker</option>
-                <option value="Dealer">Dealer</option>
-                <option value="Agent">Agent</option>
-                <option value="Realtor">Realtor</option>
-                <option value="Builder">Builder</option>
-                <option value="Coloniser">Coloniser</option>
-                <option value="Developer">Developer</option>
-                <option value="Real Estate Company">Real Estate Company</option>
-                <option value="Others">Others</option>
+                <option value="Broker"  {{@$lead->leadby === 'Broker' ? 'selected': ''}}>Broker</option>
+                <option value="Dealer"  {{@$lead->leadby === 'Dealer' ? 'selected': ''}}>Dealer</option>
+                <option value="Agent"  {{@$lead->leadby === 'Agent' ? 'selected': ''}}>Agent</option>
+                <option value="Realtor"  {{@$lead->leadby === 'Realtor' ? 'selected': ''}}>Realtor</option>
+                <option value="Builder"  {{@$lead->leadby === 'Builder' ? 'selected': ''}}>Builder</option>
+                <option value="Coloniser"  {{@$lead->leadby === 'Coloniser' ? 'selected': ''}}>Coloniser</option>
+                <option value="Developer"  {{@$lead->leadby === 'Developer' ? 'selected': ''}}>Developer</option>
+                <option value="Real Estate Company"  {{@$lead->leadby === 'Real Estate Company' ? 'selected': ''}}>Real Estate Company</option>
+                <option value="Others"  {{@$lead->leadby === 'Others' ? 'selected': ''}}>Others</option>
               </select>
         </div>
         <div class="col-3">
           <label for="leadfor" class="form-label">Lead For</label>
             <select name="leadfor" id="leadfor" class="form-select" aria-label="Default select example">
                 <option selected>Open this select menu</option>
-                <option value="buy">Buy</option>
-                <option value="sale">Sale</option>
-                <option value="rent">Rent</option>
+                <option value="buy"{{@$lead->leadfor === 'buy' ? 'selected': ''}}>Buy</option>
+                <option value="sale"{{@$lead->leadfor === 'sale' ? 'selected': ''}}>Sale</option>
+                <option value="rent"{{@$lead->leadfor === 'rent' ? 'selected': ''}}>Rent</option>
               </select>
         </div>
         <div class="col-3">
             <label for="leadtype" class="form-label">Lead Type</label>
               <select name="leadtype" id="leadtype" class="form-select" aria-label="Default select example">
                   <option selected>Open this select menu</option>
-                  <option value="Plot">Plot</option>
-                  <option value="House">House</option>
-                  <option value="Land">Land</option>
-                  <option value="Raw House">Raw House</option>
-                  <option value="Bunglow">Bunglow</option>
-                  <option value="Commercial">Commercial</option>
-                  <option value="Office">Office</option>
-                  <option value="Agriculture">Agriculture</option>
-                  <option value="Villas">Villas</option>
-                  <option value="Others">Others</option>
+                  <option value="Plot" {{@$lead->leadtype === 'Plot' ? 'selected': ''}}>Plot</option>
+                  <option value="House"  {{@$lead->leadtype === 'House' ? 'selected': ''}}>House</option>
+                  <option value="Land"  {{@$lead->leadtype === 'Land' ? 'selected': ''}}>Land</option>
+                  <option value="Raw House"  {{@$lead->leadtype === 'Raw House' ? 'selected': ''}}>Raw House</option>
+                  <option value="Bunglow"  {{@$lead->leadtype === 'Bunglow' ? 'selected': ''}}>Bunglow</option>
+                  <option value="Commercial"  {{@$lead->leadtype === 'Commercial' ? 'selected': ''}}>Commercial</option>
+                  <option value="Office"  {{@$lead->leadtype === 'Office' ? 'selected': ''}}>Office</option>
+                  <option value="Agriculture"  {{@$lead->leadtype === 'Agriculture' ? 'selected': ''}}>Agriculture</option>
+                  <option value="Villas"  {{@$lead->leadtype === 'Villas' ? 'selected': ''}}>Villas</option>
+                  <option value="Others"  {{@$lead->leadtype === 'Others' ? 'selected': ''}}>Others</option>
                 </select>
           </div>
           <div class="col-3">
             <label for="priority" class="form-label">Priority</label>
               <select name="priority" id="priority" class="form-select" aria-label="Default select example">
                   <option selected>Open this select menu</option>
-                  <option value="low">Low</option>
-                  <option value="normal">Normal</option>
-                  <option value="medium">Medium</option>
-                  <option value="high">High</option>
+                  <option value="low" {{@$lead->priority === 'low' ? 'selected': ''}}>Low</option>
+                  <option value="normal" {{@$lead->priority === 'normal' ? 'selected': ''}}>Normal</option>
+                  <option value="medium" {{@$lead->priority === 'medium' ? 'selected': ''}}>Medium</option>
+                  <option value="high" {{@$lead->priority === 'high' ? 'selected': ''}}>High</option>
                 </select>
           </div>
 
@@ -87,14 +87,14 @@
             <label for="status" class="form-label">Status</label>
               <select name="status" id="status" class="form-select" aria-label="Default select example">
                   <option selected>Open this select menu</option>
-                  <option value="Open">Open</option>
-                  <option value="Rejected">Rejected</option>
-                  <option value="Inprocess">Inprocess</option>
-                  <option value="Executed">Executed</option>
-                  <option value="Awaited">Awaited</option>
-                  <option value="Onhold">Onhold</option>
-                  <option value="Cancelled">Cancelled</option>
-                  <option value="Completed">Completed</option>
+                  <option value="Open"  {{@$lead->status === 'Open' ? 'selected': ''}}>Open</option>
+                  <option value="Rejected" {{@$lead->status === 'Rejected' ? 'selected': ''}}>Rejected</option>
+                  <option value="Inprocess" {{@$lead->status === 'Inprocess' ? 'selected': ''}}>Inprocess</option>
+                  <option value="Executed" {{@$lead->status === 'Executed' ? 'selected': ''}}>Executed</option>
+                  <option value="Awaited" {{@$lead->status === 'Awaited' ? 'selected': ''}}>Awaited</option>
+                  <option value="Onhold" {{@$lead->status === 'Onhold' ? 'selected': ''}}>Onhold</option>
+                  <option value="Cancelled" {{@$lead->status === 'Cancelled' ? 'selected': ''}}>Cancelled</option>
+                  <option value="Completed" {{@$lead->status === 'Completed' ? 'selected': ''}}>Completed</option>
                 </select>
           </div>
 
@@ -126,11 +126,11 @@
           <label for="country" class="form-label">Country</label>
           <select name="country" id="country" class="form-select">
             <option selected>Choose...</option>
-            <option>India</option>
-            <option>Pakistan</option>
-            <option>Nepal</option>
-            <option>Bhutan</option>
-            <option>SriLanka</option>
+            <option {{@$lead->country === 'India' ? 'selected': ''}}>India</option>
+            <option {{@$lead->country === 'Pakistan' ? 'selected': ''}}>Pakistan</option>
+            <option {{@$lead->country === 'Nepal' ? 'selected': ''}}>Nepal</option>
+            <option {{@$lead->country === 'Bhutan' ? 'selected': ''}}>Bhutan</option>
+            <option {{@$lead->country === 'SriLanka' ? 'selected': ''}}>SriLanka</option>
           </select>
         </div>
 
@@ -138,11 +138,11 @@
           <label for="State" class="form-label">State</label>
           <select name="state" id="State" class="form-select">
             <option selected>Choose...</option>
-            <option>Madhya Pradesh</option>
-            <option>Rajasthan</option>
-            <option>Gujrat</option>
-            <option>Punjab</option>
-            <option>Maharashtra</option>
+            <option  {{@$lead->state === 'Madhya Pradesh' ? 'selected': ''}}>Madhya Pradesh</option>
+            <option  {{@$lead->state === 'Rajasthan' ? 'selected': ''}}>Rajasthan</option>
+            <option  {{@$lead->state === 'Gujrat' ? 'selected': ''}}>Gujrat</option>
+            <option  {{@$lead->state === 'Punjab' ? 'selected': ''}}>Punjab</option>
+            <option  {{@$lead->state === 'Maharashtra' ? 'selected': ''}}>Maharashtra</option>
           </select>
         </div>
 
@@ -150,11 +150,11 @@
           <label for="city" class="form-label">City</label>
           <select name="city" id="city" class="form-select">
             <option selected>Choose...</option>
-            <option>Indore</option>
-            <option>Bhopal</option>
-            <option>Jabalpur</option>
-            <option>Ujjain</option>
-            <option>Dewas</option>
+            <option {{@$lead->city === 'Indore' ? 'selected': ''}}>Indore</option>
+            <option {{@$lead->city === 'Bhopal' ? 'selected': ''}}>Bhopal</option>
+            <option {{@$lead->city === 'Jabalpur' ? 'selected': ''}}>Jabalpur</option>
+            <option {{@$lead->city === 'Ujjain' ? 'selected': ''}}>Ujjain</option>
+            <option {{@$lead->city === 'Dewas' ? 'selected': ''}}>Dewas</option>
           </select>
         </div>
 
@@ -172,7 +172,7 @@
         </div>
         <div class="col-md-12">
           <label for="remark">Remark</label>
-          <textarea name="remark" class="form-control" placeholder="Comment Related To Lead" id="remark"></textarea>
+          <textarea name="remark" class="form-control" placeholder="Comment Related To Lead" id="remark">{{@$lead->remark}}</textarea>
         </div>
         
         <div class="col-3">
